1329415e778eb365f24fd47e9d28387243cec35a
niclaspirkl <nimaxpirkl@gmail.com>
2023-10-14 19:01:25 +0200

    Changed Waterlevel-Chart to Chart.js

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -13,8 +13,8 @@ export class HomeComponent implements OnInit, AfterViewInit {
   public percentage: number = 25;
   public litersMax: number = 4550;
   public lineChart: any;
-  public testChart: any;
-
+  public ringChart: any;
+  public ringChartPlaceholder: any;
   public percentValues: string[] = [];
   public dataValues: string[] = [];
 
@@ -31,6 +31,8 @@ export class HomeComponent implements OnInit, AfterViewInit {
       console.log(result);
       this.percentage = parseInt(result.toString()) - 10;
       this.liters = Math.round(this.litersMax * (this.percentage / 100));
+      this.createRingChart(this.percentage)
+
     });
 
     this.api.get("http://localhost:5169/HomeMatic/history").subscribe(result => {
@@ -38,14 +40,12 @@ export class HomeComponent implements OnInit, AfterViewInit {
       if (Array.isArray(result)) {
         this.percentValues = result.map(item => item.Percent.toString());
         this.dataValues = result.map(item => item.Date.toString());
-        this.createChart(this.percentValues, this.dataValues)
+        this.createLineChart(this.percentValues, this.dataValues)
 
       } else {
         console.error("Response is not an array.");
       }
     });
-
-    // this.createChart(this.percentValues, this.dataValues)
 
 
   }
@@ -59,10 +59,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
 
-
-
-
-  createChart(_percentValues: string[], _dataValues: string[]) {
+  createLineChart(_percentValues: string[], _dataValues: string[]) {
 
     this.lineChart = new Chart("lineChart", {
       type: 'line',
@@ -133,30 +130,38 @@ export class HomeComponent implements OnInit, AfterViewInit {
       }
     });
 
-    var value = 75
 
-    this.testChart = new Chart("testChart", {
+  }
+
+
+  createRingChart(percent: number) {
+
+
+    var value = percent
+
+    this.ringChart = new Chart("testChart", {
       type: 'pie',
 
       data: {
-        labels: ["",""],
+        labels: ["", ""],
         datasets: [
           {
-            data: [value, 100-value],
-            borderRadius: 20,
+            data: [value, 138 - value],
+            borderRadius: 10,
             borderColor: 'rgba(255,255,255,0.6)', // Ändern Sie die Randfarbe auf Blau
             borderWidth: 0, // Ändern Sie die Randbreite
             backgroundColor: [
-              "#ff355a",
-              "rgba(255,255,255,0.16)"
+              "#ff9900",
+              "rgba(255,255,255,0)"
             ],
           },]
       },
       options: {
+        rotation: 230,
         // aspectRatio: 1,
         responsive: true,
         // devicePixelRatio: 1,
-        cutout: 150,
+        cutout: 90,
         plugins: {
           tooltip: {
             enabled: false
@@ -164,18 +169,58 @@ export class HomeComponent implements OnInit, AfterViewInit {
           legend: {
             display: false
           }
+        },
+        animation: {
+          duration: 500, // Dauer der Animation in Millisekunden
+          easing: 'easeInOutCubic', // Art der Animation (hier linear)
         }
 
+      },
 
-      }
+    });
+
+    this.ringChartPlaceholder = new Chart("testChart1", {
+      type: 'pie',
+
+      data: {
+        labels: ["", ""],
+        datasets: [
+          {
+            data: [100, 138-100],
+            borderRadius: 10,
+            borderColor: 'rgba(255,255,255,0.6)', // Ändern Sie die Randfarbe auf Blau
+            borderWidth: 0, // Ändern Sie die Randbreite
+            backgroundColor: [
+              "rgba(255,255,255,0.12)",
+              "rgba(255,255,255,0)"
+            ],
+          },]
+      },
+      options: {
+        rotation: 230,
+        // aspectRatio: 1,
+        responsive: true,
+        // devicePixelRatio: 1,
+        cutout: 90,
+        plugins: {
+          tooltip: {
+            enabled: false
+          },
+          legend: {
+            display: false
+          }
+        },
+        animation: {
+          duration: 500, // Dauer der Animation in Millisekunden
+          easing: 'easeInOutCubic', // Art der Animation (hier linear)
+        }
+
+      },
+
     });
 
 
-
-    this.testChart.canvas.style.height = '128px';
-    this.testChart.canvas.style.width = '128px';
-
-
   }
+
 
 }
